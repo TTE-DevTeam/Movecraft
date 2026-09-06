0a0316f1699c43564abadd68a8257bf8530299d7
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-09-06 14:45:46 +0200

    use correct register() method in SignRegistry

--- api/src/main/java/net/countercraft/movecraft/sign/MovecraftSignRegistry.java
+++ api/src/main/java/net/countercraft/movecraft/sign/MovecraftSignRegistry.java
@@ -19,7 +19,7 @@ public class MovecraftSignRegistry extends SimpleRegistry<String, AbstractMovecr
         // Now, add all types...
         for (TypeSafeCraftType type : loadedTypes) {
             AbstractCraftPilotSign sign = signFactory.apply(type);
-            register(type.getSignHeader(), sign, true);
+            register(type.getSignHeader(), sign, true, new String[0]);
         }
     }
 
